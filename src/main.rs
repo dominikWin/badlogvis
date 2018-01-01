575f1cbbc6f7039f1711cda35e5a34d983098b8d
Dominik Winecki <dominikwinecki@gmail.com>
2017-12-31 21:50:52 -0500

    Add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "badlogvis", about = "Create html from badlog data")]
+struct Opt {
+    #[structopt(help = "Input file")]
+    input: String,
+
+    #[structopt(help = "Output file, default to <input>.html")]
+    output: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt: Opt = Opt::from_args();
+    println!("{:?}", opt);
 }
