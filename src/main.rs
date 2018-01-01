325e61c9528125743d63ea37fb40ff08f58715e3
Dominik Winecki <dominikwinecki@gmail.com>
2017-12-31 21:59:55 -0500

    Add file reading

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@ extern crate structopt;
 #[macro_use]
 extern crate structopt_derive;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
@@ -17,4 +20,16 @@ struct Opt {
 fn main() {
     let opt: Opt = Opt::from_args();
     println!("{:?}", opt);
+
+    let input = opt.input;
+
+    let mut f = File::open(input).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).unwrap();
+
+    println!("{}", contents);
+
+    let json_header = contents.lines().take(1).last().unwrap().to_string();
+
+    println!("{}", json_header);
 }
