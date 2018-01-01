e707d6b6e3cce55c47dc608d1d1b7a29b999e314
Dominik Winecki <dominikwinecki@gmail.com>
2018-01-01 01:14:49 -0500

    Add CSV parsing

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+extern crate tempfile;
+extern crate csv;
 
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::SeekFrom;
 
 use structopt::StructOpt;
 
@@ -40,18 +43,82 @@ struct JSONHeader {
     values: Vec<Value>
 }
 
+#[derive(Debug)]
+struct Graph {
+    pub name: String,
+    pub unit: String,
+    pub attrs: Vec<String>,
+    pub data: Vec<(f64, f64)>
+}
+
 fn main() {
     let opt: Opt = Opt::from_args();
 
     let input = opt.input;
 
-    let mut f = File::open(input).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).unwrap();
+    let contents: String = {
+        let mut f = File::open(input).expect("file not found");
+        let mut contents = String::new();
+        f.read_to_string(&mut contents).unwrap();
+        contents
+    };
 
     let json_header = contents.lines().take(1).last().unwrap().to_string();
+    let csv_data = contents.lines().skip(1).fold("".to_string(), |a, b| {
+        if a.len() == 0 {
+            b.to_string()
+        } else {
+            [a,b.to_string()].join("\n")
+        }
+    });
 
     let p: JSONHeader = serde_json::from_str(&json_header).unwrap();
 
     println!("{:?}", p);
+
+    let mut tempfile = tempfile::tempfile().unwrap();
+    tempfile.write(csv_data.as_bytes()).unwrap();
+    tempfile.seek(SeekFrom::Start(0)).unwrap();
+
+    let rdr = csv::Reader::from_reader(tempfile);
+
+    let graphs = gen_graphs(p, rdr);
+
+    println!("{:?}", graphs);
+}
+
+fn gen_graphs(json_header: JSONHeader, mut csv_reader: csv::Reader<File>) -> Vec<Graph> {
+    let mut out: Vec<Graph> = Vec::new();
+
+    for topic in json_header.topics {
+        let mut graph = Graph {
+            name: topic.name,
+            unit: topic.unit,
+            attrs: topic.attrs,
+            data: Vec::new()
+        };
+        out.push(graph);
+    }
+
+    let header = {
+        csv_reader.headers().unwrap().clone()
+    };
+
+    let mut step = 0;
+    for row in csv_reader.records() {
+        let row = row.unwrap();
+        assert_eq!(row.len(), header.len());
+
+        for i in 0..header.len() {
+            let (k, v) = (&header[i], &row[i]);
+
+            assert_eq!(out.iter().filter(|g| g.name.eq(k)).count(), 1);
+            let graph = out.iter_mut().filter(|g| g.name.eq(k)).last().unwrap();
+            graph.data.push(((step as f64), v.to_string().parse::<f64>().unwrap()));
+        }
+
+        step += 1;
+    }
+
+    out
 }
