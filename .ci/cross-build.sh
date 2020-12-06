09db92a4c8013c8b068d215fcbd9f2063e3e6e7b
Richik SC <sc.richik@gmail.com>
2020-12-06 12:26:45 -0600

    Add Linux and Windows build to workflow

--- .ci/cross-build.sh
+++ .ci/cross-build.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 if [[ -n "$WIN" ]]; then
-  rustup target add $WIN &&
+  sudo apt-get install gcc-mingw-w64 &&
   printf "[target.$WIN]\nlinker = \"x86_64-w64-mingw32-gcc\"" >> ~/.cargo/config &&
   cargo build --release --target=$WIN &&
-  mv target/$WIN/release/badlogvis.exe target/$WIN/release/badlogvis-$WIN.exe
-fi
-
-if [[ -n "$ARM" ]]; then
-  rustup target add $ARM &&
-  SDKROOT=$(xcrun -sdk macosx11.0 --show-sdk-path) \
-  MACOSX_DEPLOYMENT_TARGET=$(xcrun -sdk macosx11.0 --show-sdk-platform-version) \
-  cargo build --release --target=$ARM &&
-  mv target/$ARM/release/badlogvis target/$ARM/release/badlogvis-$ARM
+  mv target/$WIN/release/badlogvis.exe target/release/badlogvis-$WIN.exe
 fi
