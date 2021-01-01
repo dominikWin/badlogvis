d1de353cdcb45146681d4f2b37ccc4dbb8f2652a
Richik SC <sc.richik@gmail.com>
2021-01-01 12:26:33 -0600

    Switch to stable Rust now that v1.49 is released, simplify GitHub workflow

--- .ci/package.sh
+++ .ci/package.sh
@@ -4,16 +4,21 @@ mkdir -p out/badlogvis-$TARGET
 cp target/release/badlogvis out/badlogvis-$TARGET/
 cp .ci/install.sh out/badlogvis-$TARGET/
 
-if [[ -n "$ARM" ]]; then
-  mkdir -p out/badlogvis-$ARM
-  cp target/$ARM/release/badlogvis out/badlogvis-$ARM/
-  cp .ci/install.sh out/badlogvis-$ARM/
+mkdir -p out/badlogvis-$SECONDARY
+
+if [[ "$SECONDARY" != "x86_64-pc-windows-gnu" ]]; then
+  cp .ci/install.sh out/badlogvis-$SECONDARY/
+  cp target/$SECONDARY/release/badlogvis out/badlogvis-$SECONDARY/
+else
+  cp target/$SECONDARY/release/badlogvis.exe out/badlogvis-$SECONDARY/
 fi
 
 cd out/
 tar -czvf badlogvis-$TARGET.tar.gz badlogvis-$TARGET/
 
-if [[ -n "$ARM" ]]; then
-  tar -czvf badlogvis-$ARM.tar.gz badlogvis-$ARM/
+if [[ "$SECONDARY" != "x86_64-pc-windows-gnu" ]]; then
+  tar -czvf badlogvis-$SECONDARY.tar.gz badlogvis-$SECONDARY/
+else
+  zip -r badlogvis-$SECONDARY.zip badlogvis-$SECONDARY/
 fi
 
